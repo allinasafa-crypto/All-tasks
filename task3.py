e13cbe17af2da961dfb170a85d82ed90cccadd2e
allinasafa-crypto <allinasafa@gmail.com>
2025-08-31 19:14:19 +0500

    Added task3.py and task4.py

---
+++ task3.py
@@ -0,0 +1,23 @@
+# Task 1: String Slicing Practice
+# Create a string name = "Programming"
+# # Print the following:
+ 
+# # First 5 letters
+name = "Programming"
+print(name[0:5])
+
+# # Last 3 letters
+name = "Programming"
+print(name[-3:])
+
+# # Letters from index 2 to 6
+name = "Programming"
+print(name[2:7])
+
+# # Whole string except the last 2 letters
+name = "Programming"
+print(name[0:9])
+
+# # The length of the string
+name = "Programming"
+print(len(name))
---
+++ task4.py
@@ -0,0 +1,22 @@
+# Task 2: String Methods and Escape Characters
+# Create a string sentence = "Python is Fun".
+# Print the following:
+sentence = "Python is Fun"
+
+# Does the string end with "Fun"?
+print(sentence.endswith("Fun"))
+
+# Does the string start with "Python"?
+print(sentence.startswith("Python"))
+
+# The capitalized form of the string
+print(sentence.capitalize())
+
+# The length of the string
+print(len(sentence))
+
+# Create another string using escape characters:
+
+# "He said, \"Python is awesome!\"\nBut I said, \'It is challenging.\'"
+text = "He said, \"Python is awesome!\"\nBut I said, \'It is challenging.\'"
+print(text)
