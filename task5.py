e9ab6a395ec6b22889a92acc85a47a5e1abd9bf9
allinasafa-crypto <allinasafa@gmail.com>
2025-09-08 23:11:18 +0500

    first commit

---
+++ task5.py
@@ -0,0 +1,14 @@
+# Task 1
+items = ["Pen", "Book", 12, 45.6, False]
+
+# "Pen" replace by "Pencil"
+items[0] = "Pencil"
+
+# At Last add "Eraser" in list
+items.append("Eraser")
+
+# remove Index 2 
+items.pop(2)
+
+# Print List slicing [1:4] 
+print(items[1:4])
---
+++ task6.py
@@ -0,0 +1,12 @@
+# Task 2
+t = (1, 2, 5, 2, "Python", True)
+
+# Tuple print and type check 
+print("Tuple:", t)
+print("Type:", type(t))
+
+# Tuple mein 2 kitni dafa aya hai
+print("2 times:", t.count(2))
+
+# "Python" ka index
+print("Python's index:", t.index("Python"))
